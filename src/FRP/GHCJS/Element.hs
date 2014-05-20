39f3277edc108150453c84272659da57268c54e8
Kyle Raftogianis <kylerafto@gmail.com>
2014-05-20 14:07:22 -0700

    Documentation

--- src/FRP/GHCJS/Element.hs
+++ src/FRP/GHCJS/Element.hs
@@ -8,13 +8,16 @@ module FRP.GHCJS.Element
 import           Data.Text      (Text)
 import qualified GHCJS.DOM.Node as DOM
 
--- | A DOM node.
+-- | A document element.
 data Element
+      -- | Extend an 'Element' with initialization and update operations.
     = Extend Component Element
+      -- | A vanilla HTML tag.
     | Tag Text [Element]
+      -- | A text node.
     | Text Text
 
--- | An HTML component.
+-- | A logical component in the document.
 data Component = Component
     { -- | A component name that uniquely identifies the type or class of
       -- this component. 'update' and 'delete' may assume that the 'DOM.Node'
