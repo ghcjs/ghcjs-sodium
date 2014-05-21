7defdfa98bd3919376c71f155dd29eca39ca5222
Kyle Raftogianis <kylerafto@gmail.com>
2014-05-21 15:59:37 -0700

    Global attributes

--- src/FRP/GHCJS/Attributes.hs
+++ src/FRP/GHCJS/Attributes.hs
@@ -1,16 +1,20 @@
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes        #-}
+{-# LANGUAGE TemplateHaskell   #-}
 -- | HTML attributes and properties.
 module FRP.GHCJS.Attributes where
 
-import           Control.Applicative
 import           Control.Lens
 import           Control.Monad
-import           Data.Foldable       as Foldable
-import           Data.Monoid
-import           Data.Text           as Text
-import qualified GHCJS.DOM.Element   as DOM
-import qualified GHCJS.DOM.Node      as DOM
+import           Data.Foldable         as Foldable
+import           Data.HashSet
+import           Data.Text             as Text
+import           Data.Set
+import           GHC.Generics          (Generic)
+import qualified GHCJS.DOM.Element     as DOM
+import qualified GHCJS.DOM.HTMLElement as DOM
+import qualified GHCJS.DOM.Node        as DOM
 
 import           FRP.GHCJS.Default
 
@@ -24,10 +28,11 @@ attribute
     :: DOM.IsElement e
     => a
     -> e
+    -> Getting (Maybe Text) a b
     -> Text
-    -> Getting (First Text) a Text
+    -> (b -> Maybe Text)
     -> IO ()
-attribute a e attr l = case a ^? l of
+attribute a e l attr f = case views l f a of
     Nothing    -> DOM.elementRemoveAttribute e attr
     Just value -> DOM.elementSetAttribute e attr value
 
@@ -35,36 +40,112 @@ attribute a e attr l = case a ^? l of
 property
     :: a
     -> e
+    -> Getting b a c
     -> (e -> b -> IO ())
-    -> Getting b a b
+    -> (c -> b)
     -> IO ()
-property a e setProp l = setProp e (a ^. l)
+property a e l setProp f = setProp e (views l f a)
+
+-- | Set a property on an element, and remove it by removing the attribute.
+hybridProperty
+    :: DOM.IsElement e
+    => a
+    -> e
+    -> Getting (Maybe b) a c
+    -> (e -> b -> IO ())
+    -> Text
+    -> (c -> Maybe b)
+    -> IO ()
+hybridProperty a e l setProp attr f = case views l f a of
+    Nothing    -> DOM.elementRemoveAttribute e attr
+    Just value -> setProp e value
 
 -- | Set a property on an element if the value has not changed. Use this for
--- properties such as @value@ that hav side-effects when set.
+-- properties such as @value@ that have side-effects when set.
 safeProperty
     :: Eq b
     => a
     -> e
+    -> Getting b a c
     -> (e -> IO b)
     -> (e -> b -> IO ())
-    -> Getting b a b
+    -> (c -> b)
     -> IO ()
-safeProperty a e getProp setProp l = do
-    let new = a ^. l
+safeProperty a e l getProp setProp f = do
+    let new = views l f a
     old <- getProp e
     when (old /= new) $ setProp e new
 
--- | Convert a boolean attributes.
-boolean :: Text -> Fold Bool Text
-boolean name = folding $ \b -> if b then Just name else Nothing
+-- | Convert a boolean attribute.
+boolean :: Text -> Bool -> Maybe Text
+boolean name b = if b then Just name else Nothing
+
+-- | Convert a ternary attribute, which may be @true@, @false@, or a third
+-- state.
+ternary :: Text -> Maybe Bool -> Text
+ternary third = maybe third (\b -> if b then "true" else "false")
 
 -- | Convert a non-empty text attribute.
-nonEmpty :: Fold Text Text
-nonEmpty = filtered (not . Text.null)
+nonNull :: Text -> Maybe Text
+nonNull t = if Text.null t then Nothing else Just t
 
 -- | Separate the elements of a container with spaces.
-spaceSep :: Foldable f => Getting Text a Text -> Fold (f a) Text
-spaceSep l = folding $ \f -> case view l <$> Foldable.toList f of
-    [] -> Nothing
-    ts -> Just (Text.intercalate " " ts)
+spaceSep :: Foldable f => (a -> Text) -> f a -> Text
+spaceSep f = Text.intercalate " " . fmap f . Foldable.toList
+
+-- | An element identifier.
+type ElementId = Text
+
+-- | Text directionality.
+data Dir = LTR | RTL | Auto
+    deriving (Eq, Ord, Read, Show, Enum, Bounded)
+
+-- | Element style.
+type Style = ()
+
+-- | Global attributes.
+data GlobalAttributes = GlobalAttributes
+    { _accessKey       :: Set Char
+    , _className       :: HashSet Text
+    , _contentEditable :: Maybe Bool
+    , _contextMenu     :: ElementId
+    , _dir             :: Maybe Dir
+    , _draggable       :: Maybe Bool
+    , _hidden          :: Bool
+    , __id             :: ElementId
+    , _lang            :: Text
+    , _spellCheck      :: Bool
+    , _style           :: Style
+    , _tabIndex        :: Maybe Int
+    , _title           :: Text
+    } deriving (Eq, Show, Generic)
+
+makeClassy ''GlobalAttributes
+
+instance Default GlobalAttributes
+
+instance Attributes GlobalAttributes where
+    applyAttributes a n = do
+        prop    accessKey       DOM.htmlElementSetAccessKey             (spaceSep Text.singleton)
+        prop    className       DOM.elementSetClassName                 (spaceSep id)
+        prop    contentEditable DOM.htmlElementSetContentEditable       (ternary "inherit")
+        attr    contextMenu     "contextmenu"                           nonNull
+        hybrid  dir             DOM.htmlElementSetDir "dir"             (fmap dirValue)
+        hybrid  draggable       DOM.htmlElementSetDraggable "draggable" id
+        attr    hidden          "hidden"                                (boolean "hidden")
+        prop    _id             DOM.elementSetId                        id
+        prop    lang            DOM.htmlElementSetLang                  id
+        prop    spellCheck      DOM.htmlElementSetSpellcheck            id
+        -- TODO: style
+        hybrid  tabIndex        DOM.htmlElementSetTabIndex "tabindex"   id
+        prop    title           DOM.htmlElementSetTitle                 id
+      where
+        e      = DOM.castToHTMLElement n
+        attr   = attribute a e
+        prop   = property a e
+        hybrid = hybridProperty a e
+
+        dirValue :: Dir -> Text
+        dirValue LTR  = "ltr"
+        dirValue RTL  = "rtl"
+        dirValue Auto = "auto"
