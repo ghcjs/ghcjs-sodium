cbb6e437289d52af959d2ae07f991df4d1def279
Kyle Raftogianis <kylerafto@gmail.com>
2014-05-23 15:37:25 -0700

    Dispatch events

--- src/FRP/GHCJS/Dispatch.hs
+++ src/FRP/GHCJS/Dispatch.hs
@@ -13,9 +13,14 @@ module FRP.GHCJS.Dispatch
 
 import           Control.Applicative
 import           Control.Lens
-import           Control.Monad.IO.Class
+import           Control.Monad
 import           Control.Monad.State.Class
+import           Control.Monad.Trans
+import           Control.Monad.Trans.Maybe
 import           Data.HashMap.Strict       (HashMap)
+import qualified Data.HashMap.Strict       as HashMap
+import           Data.Text                 (Text)
+import           FRP.Sodium
 import           GHC.Generics
 import qualified GHCJS.DOM.Element         as DOM
 import qualified GHCJS.DOM.Event           as DOM
@@ -23,6 +28,7 @@ import           GHCJS.Types
 
 import           Data.Default
 import           FRP.GHCJS.Events
+import           FRP.GHCJS.Input
 
 -- | A node's unique identifier (for this library's purposes).
 type Name = Int
@@ -38,7 +44,7 @@ makeClassy ''Dispatch
 instance Default Dispatch
 
 -- | Monads that can access the dispatch state.
-class (MonadIO m, MonadState s m, HasDispatch s) => Dispatches s m
+class (Functor m, MonadIO m, MonadState s m, HasDispatch s) => Dispatches s m
 
 -- | Get the conanical name of an element.
 getName :: Dispatches s m => DOM.Element -> m Name
@@ -66,6 +72,35 @@ unregister e = do
     name <- getName e
     eventMap . at name .= Nothing
 
+
 -- | Dispatch a 'DOM.Event' with a type name to the appropriate element.
-dispatchEvent :: Dispatches s m => JSString -> DOM.Event -> m ()
-dispatchEvent = undefined
+dispatchEvent :: Dispatches s m => Text -> DOM.Event -> m ()
+dispatchEvent evType ev = void . runMaybeT $ do
+    target <- MaybeT . liftIO $ DOM.eventGetTarget ev
+    name   <- lift $ getName (DOM.castToElement target)
+    is     <- MaybeT $ use (eventMap . at name)
+    action <- MaybeT . return $ HashMap.lookup evType selectors
+    liftIO $ action is ev
+
+-- | Select an event to fire.
+select
+    :: (Getting (Input e) a (Input e))
+    -> (DOM.Event -> IO e)
+    -> a
+    -> DOM.Event
+    -> IO ()
+select l extract a ev = do
+    e <- extract ev
+    sync $ fire (a ^. l) e
+
+-- | Extract a mouse event.
+mouseEvent :: DOM.Event -> IO MouseEvent
+mouseEvent _ = return MouseEvent
+
+-- | Event selectors.
+selectors :: HashMap Text (Events -> DOM.Event -> IO ())
+selectors = HashMap.fromList
+    [ "click" ==> select click mouseEvent
+    ]
+  where
+    (==>) = (,)
