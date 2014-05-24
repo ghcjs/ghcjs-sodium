9758fc27286160501ec5b3b83f0f139290a33baf
Kyle Raftogianis <kylerafto@gmail.com>
2014-05-23 21:24:21 -0700

    Extract mouse events

--- src/FRP/GHCJS/Dispatch.hs
+++ src/FRP/GHCJS/Dispatch.hs
@@ -20,15 +20,16 @@ import           Control.Monad.Trans.Maybe
 import           Data.HashMap.Strict       (HashMap)
 import qualified Data.HashMap.Strict       as HashMap
 import           Data.Text                 (Text)
-import           FRP.Sodium
+import           FRP.Sodium                (sync)
 import           GHC.Generics
 import qualified GHCJS.DOM.Element         as DOM
 import qualified GHCJS.DOM.Event           as DOM
 import           GHCJS.Types
 
 import           Data.Default
-import           FRP.GHCJS.Events
+import qualified FRP.GHCJS.Events          as E
 import           FRP.GHCJS.Input
+import           FRP.GHCJS.Internal.Events
 
 -- | A node's unique identifier (for this library's purposes).
 type Name = Int
@@ -36,7 +37,7 @@ type Name = Int
 -- | The event dispatch system state.
 data Dispatch = Dispatch
     { _nextName :: !Name
-    , _eventMap :: !(HashMap Name Events)
+    , _eventMap :: !(HashMap Name E.Events)
     } deriving (Generic)
 
 makeClassy ''Dispatch
@@ -61,7 +62,7 @@ getName e = do
 
 -- | Register a set of event handlers for an element, overwriting any
 -- previous handlers.
-register :: Dispatches s m => DOM.Element -> Events -> m ()
+register :: Dispatches s m => DOM.Element -> E.Events -> m ()
 register e evs = do
     name <- getName e
     eventMap . at name ?= evs
@@ -71,7 +72,6 @@ unregister :: Dispatches s m => DOM.Element -> m ()
 unregister e = do
     name <- getName e
     eventMap . at name .= Nothing
-
 
 -- | Dispatch a 'DOM.Event' with a type name to the appropriate element.
 dispatchEvent :: Dispatches s m => Text -> DOM.Event -> m ()
@@ -84,23 +84,35 @@ dispatchEvent evType ev = void . runMaybeT $ do
 
 -- | Select an event to fire.
 select
-    :: (Getting (Input e) a (Input e))
-    -> (DOM.Event -> IO e)
+    :: Event e
+    => (Getting (Input e) a (Input e))
     -> a
     -> DOM.Event
     -> IO ()
-select l extract a ev = do
-    e <- extract ev
+select l a ev = do
+    e <- extractEvent ev
     sync $ fire (a ^. l) e
 
--- | Extract a mouse event.
-mouseEvent :: DOM.Event -> IO MouseEvent
-mouseEvent _ = return MouseEvent
-
 -- | Event selectors.
-selectors :: HashMap Text (Events -> DOM.Event -> IO ())
+selectors :: HashMap Text (E.Events -> DOM.Event -> IO ())
 selectors = HashMap.fromList
-    [ "click" ==> select click mouseEvent
+    [ "click"       ==> select E.click
+    , "doubleClick" ==> select E.doubleClick
+    , "drag"        ==> select E.drag
+    , "dragEnd"     ==> select E.dragEnd
+    , "dragEnter"   ==> select E.dragEnter
+    , "dragExit"    ==> select E.dragExit
+    , "dragLeave"   ==> select E.dragLeave
+    , "dragOver"    ==> select E.dragOver
+    , "dragStart"   ==> select E.dragStart
+    , "drop"        ==> select E.drop
+    , "mouseDown"   ==> select E.mouseDown
+    , "mouseEnter"  ==> select E.mouseEnter
+    , "mouseLeave"  ==> select E.mouseLeave
+    , "mouseMove"   ==> select E.mouseMove
+    , "mouseOut"    ==> select E.mouseOut
+    , "mouseOver"   ==> select E.mouseOver
+    , "mouseUp"     ==> select E.mouseUp
     ]
   where
     (==>) = (,)
--- src/FRP/GHCJS/Events.hs
+++ src/FRP/GHCJS/Events.hs
@@ -1,24 +1,102 @@
-{-# LANGUAGE DeriveGeneric   #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell       #-}
 -- | Events.
 module FRP.GHCJS.Events
-    ( MouseEvent(..)
-    , Events
+    ( Events
     , HasEvents(..)
+    , MouseEvent
+    , HasMouseEvent(..)
+    , Button(..)
+    , Modifier(..)
+    , Position(..)
     ) where
 
-import           Control.Lens.TH
+import           Control.Applicative
+import           Control.Lens
+import           Data.Maybe
+import           Data.Set                  (Set)
+import qualified Data.Set                  as Set
 import           GHC.Generics
+import qualified GHCJS.DOM.MouseEvent      as DOM
 
 import           Data.Default
 import           FRP.GHCJS.Input
+import           FRP.GHCJS.Internal.Events
+
+data Button = LeftButton | MiddleButton | RightButton
+    deriving (Eq, Ord, Read, Show, Enum, Bounded)
+
+data Modifier = Alt | Ctrl | Meta | Shift
+    deriving (Eq, Ord, Read, Show, Enum, Bounded)
+
+data Position = Position !Int !Int
+    deriving (Eq, Ord, Read, Show, Generic)
+
+instance Field1 Position Position Int Int where
+    _1 k (Position x y) = indexed k (0 :: Int) x <&> \x' -> Position x' y
+
+instance Field2 Position Position Int Int where
+    _2 k (Position x y) = indexed k (1 :: Int) y <&> \y' -> Position x y'
 
 -- | A mouse event.
 data MouseEvent = MouseEvent
+    { _button         :: !Button
+    , _modifiers      :: !(Set Modifier)
+    , _clientPosition :: !Position
+    , _pagePosition   :: !Position
+    , _screenPosition :: !Position
+    }
+
+makeClassy ''MouseEvent
+
+instance Event MouseEvent where
+    extractEvent ev = do
+        let e = DOM.castToMouseEvent ev
+        evButton <- DOM.mouseEventGetButton e <&> \i -> case i of
+            2 -> RightButton
+            1 -> MiddleButton
+            _ -> LeftButton
+        let switch a b = if b then Just a else Nothing
+        evModifiers <- Set.fromList . catMaybes <$> sequence
+            [ switch Alt   <$> DOM.mouseEventGetAltKey e
+            , switch Ctrl  <$> DOM.mouseEventGetCtrlKey e
+            , switch Meta  <$> DOM.mouseEventGetMetaKey e
+            , switch Shift <$> DOM.mouseEventGetShiftKey e
+            ]
+        clientPos <- Position <$> DOM.mouseEventGetClientX e
+                              <*> DOM.mouseEventGetClientY e
+        pagePos   <- Position <$> DOM.mouseEventGetX e
+                              <*> DOM.mouseEventGetY e
+        screenPos <- Position <$> DOM.mouseEventGetScreenX e
+                              <*> DOM.mouseEventGetScreenY e
+        return MouseEvent
+            { _button         = evButton
+            , _modifiers      = evModifiers
+            , _clientPosition = clientPos
+            , _pagePosition   = pagePos
+            , _screenPosition = screenPos
+            }
 
 -- | A set of event handlers for an element.
 data Events = Events
-    { _click :: Input MouseEvent
+    { _click       :: Input MouseEvent
+    , _doubleClick :: Input MouseEvent
+    , _drag        :: Input MouseEvent
+    , _dragEnd     :: Input MouseEvent
+    , _dragEnter   :: Input MouseEvent
+    , _dragExit    :: Input MouseEvent
+    , _dragLeave   :: Input MouseEvent
+    , _dragOver    :: Input MouseEvent
+    , _dragStart   :: Input MouseEvent
+    , _drop        :: Input MouseEvent
+    , _mouseDown   :: Input MouseEvent
+    , _mouseEnter  :: Input MouseEvent
+    , _mouseLeave  :: Input MouseEvent
+    , _mouseMove   :: Input MouseEvent
+    , _mouseOut    :: Input MouseEvent
+    , _mouseOver   :: Input MouseEvent
+    , _mouseUp     :: Input MouseEvent
     } deriving (Generic)
 
 instance Default Events
