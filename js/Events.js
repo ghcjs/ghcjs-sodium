c0b45552d79a59049c5dd1e32e3a4b22415c94f6
Kyle Raftogianis <kylerafto@gmail.com>
2014-06-02 13:43:48 -0700

    Listen to mouse events

---
+++ js/Events.js
@@ -0,0 +1,51 @@
+var Events = (function() {
+
+    function trapEvent(eventName, extractor, callback) {
+        document.addEventListener(eventName, function(e) {
+            var obj = extractor(eventName, e);
+            callback(e.target, eventName, obj);
+        });
+    }
+
+    function extractMouseEvent(eventName, event) {
+        var button = event.button;
+        if (!('which' in event)) {
+            // IE<9
+            button = button === 2 ? 2 : button === 4 ? 1 : 0;
+        }
+
+        var pageX = 'pageX' in event ?
+            event.pageX :
+            event.clientX + document.body.scrollLeft +
+                document.documentElement.scrollLeft;
+
+        var pageY = 'pageY' in event ?
+            event.pageY :
+            event.clientY + document.body.scrollTop +
+                document.documentElement.scrollTop;
+
+        return {
+            button:   button,
+            altKey:   event.altKey,
+            ctrlKey:  event.ctrlKey,
+            metaKey:  event.metaKey,
+            shiftKey: event.shiftKey,
+            clientX:  event.clientX,
+            clientX:  event.clientX,
+            pageX:    pageX,
+            pageY:    pageY,
+            screenX:  event.screenX,
+            screenX:  event.screenY
+        };
+    }
+
+    return {
+        listen: function (callback) {
+            trapEvent('click', extractMouseEvent, callback);
+            trapEvent('dblclick', extractMouseEvent, callback);
+            trapEvent('mousedown', extractMouseEvent, callback);
+            trapEvent('mouseup', extractMouseEvent, callback);
+        }
+    };
+
+}());
