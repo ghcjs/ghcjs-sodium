0b5c19c2ddd1c94ffc7856d1acfbedb2b1e28b27
Kyle Raftogianis <kylerafto@gmail.com>
2014-06-03 18:11:17 -0700

    Better sodium functions

--- src/Alder/Sodium.hs
+++ src/Alder/Sodium.hs
@@ -1,8 +1,9 @@
 module Alder.Sodium
     ( Input(..)
     , newInput
-    , filterInput
     , mount
+    , keep
+    , pass
     , select
     , (<@>)
     , (<@)
@@ -35,21 +36,24 @@ instance Handler Input where
 newInput :: Reactive (Event a, Input a)
 newInput = second Input <$> newEvent
 
-filterInput :: (a -> Bool) -> Input a -> Input a
-filterInput p (Input f) = Input $ \a -> if p a then f a else return ()
-
 mount :: Behavior Html -> IO ()
 mount b = do
     push <- Alder.mount
     void . sync $ listen (value b) push
 
-select
-    :: ((b -> Const (Endo [b]) b) -> a -> Const (Endo [b]) a)
-    -> Event a
-    -> Event b
-select l = split . fmap toList
-  where
-    toList = flip appEndo [] . getConst . l (Const . Endo . (:))
+type Getting r s a = (a -> Const r a) -> s -> Const r s
+
+toListOf :: Getting (Endo [b]) a b -> a -> [b]
+toListOf l = flip appEndo [] . getConst . l (Const . Endo . (:))
+
+keep :: (a -> Bool) -> Input a -> Input a
+keep p (Input f) = Input $ \a -> if p a then f a else return ()
+
+pass :: Getting (Endo [b]) a b -> Input b -> Input a
+pass l (Input f) = Input $ \a -> mapM_ f (toListOf l a)
+
+select :: Getting (Endo [b]) a b -> Event a -> Event b
+select l = split . fmap (toListOf l)
 
 (<@>) :: Behavior (a -> b) -> Event a -> Event b
 b <@> e = snapshot (flip ($)) e b
