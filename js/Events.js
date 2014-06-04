2fe8be96b1c1af5bdeb68db97f52bc9b94f23ac8
Kyle Raftogianis <kylerafto@gmail.com>
2014-06-04 16:40:20 -0700

    Drop support for IE8 (for now)

--- js/Events.js
+++ js/Events.js
@@ -1,19 +1,9 @@
 var Events = (function() {
     "use strict";
 
-    var isOldIE = !('addEventListener' in document);
-
-    function listenTo(eventName, handler, useCapture) {
-        if (isOldIE) {
-            document.attachEvent('on' + eventName, handler);
-        } else {
-            document.addEventListener(eventName, handler, useCapture);
-        }
-    }
-
     function trapEvent(eventName, extractor, useCapture) {
         return function (callback) {
-            listenTo(eventName, function(e) {
+            document.addEventListener(eventName, function(e) {
                 var obj = extractor(e);
                 callback({
                     target: e.target,
@@ -26,9 +16,8 @@ var Events = (function() {
 
     function trapKeyboardEvent(eventName) {
         return trapEvent(eventName, function (e) {
-            var key = e.keyCode || e.charCode;
             return {
-                key:      key,
+                key:      e.keyCode || e.charCode,
                 altKey:   e.altKey,
                 ctrlKey:  e.ctrlKey,
                 metaKey:  e.metaKey,
@@ -40,15 +29,7 @@ var Events = (function() {
         });
     }
 
-    function trapFocusEvent() {
-        var eventName = isOldIE ? 'focusin' : 'focus';
-        return trapEvent(eventName, function () {
-            return {};
-        }, true);
-    }
-
-    function trapBlurEvent() {
-        var eventName = isOldIE ? 'focusout' : 'blur';
+    function trapCapturedEvent(eventName) {
         return trapEvent(eventName, function () {
             return {};
         }, true);
@@ -57,46 +38,28 @@ var Events = (function() {
     function trapInputEvent() {
         return trapEvent('input', function (e) {
             var target = e.target;
-            return {
-                checked: target.checked,
-                value:   target.value
-            };
-        });
-    }
+            var checked = target.checked ? true : false;
+            var value = target.value ? target.value : '';
 
-    function trapSubmitEvent() {
-        return trapEvent('submit', function () {
-            return {};
+            return {
+                checked: checked,
+                value:   value
+            };
         });
     }
 
     function trapMouseEvent(eventName) {
         return trapEvent(eventName, function (e) {
-            var button = e.button;
-            if (isOldIE) {
-                button = button === 2 ? 2 : button === 4 ? 1 : 0;
-            }
-
-            var pageX = 'pageX' in e ?
-                e.pageX :
-                e.clientX + document.body.scrollLeft +
-                    document.documentElement.scrollLeft;
-
-            var pageY = 'pageY' in e ?
-                e.pageY :
-                e.clientY + document.body.scrollTop +
-                    document.documentElement.scrollTop;
-
             return {
-                button:   button,
+                button:   e.button,
                 altKey:   e.altKey,
                 ctrlKey:  e.ctrlKey,
                 metaKey:  e.metaKey,
                 shiftKey: e.shiftKey,
                 clientX:  e.clientX,
                 clientY:  e.clientY,
-                pageX:    pageX,
-                pageY:    pageY,
+                pageX:    e.pageX,
+                pageY:    e.pageY,
                 screenX:  e.screenX,
                 screenY:  e.screenY
             };
@@ -107,10 +70,10 @@ var Events = (function() {
         keydown   : trapKeyboardEvent('keydown'),
         keypress  : trapKeyboardEvent('keypress'),
         keyup     : trapKeyboardEvent('keyup'),
-        focus     : trapFocusEvent(),
-        blur      : trapBlurEvent(),
+        focus     : trapCapturedEvent('focus'),
+        blur      : trapCapturedEvent('blur'),
         input     : trapInputEvent(),
-        submit    : trapSubmitEvent(),
+        submit    : trapCapturedEvent('submit'),
         mousedown : trapMouseEvent('mousedown'),
         mouseup   : trapMouseEvent('mouseup'),
         click     : trapMouseEvent('click'),
